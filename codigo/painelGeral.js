6883ff1383975f92c026f6253bb565b3ca519551
leticialott <103859841+leticialott@users.noreply.github.com>
2022-06-28 20:31:28 -0300

    Add files via upload

--- codigo/painelGeral.js
+++ codigo/painelGeral.js
@@ -5,30 +5,29 @@ var jsonObject  = {
       {
         "title": "10% OFF",
         "description": "10% de desconto em todos os produtos! Válido entre 30/05/2022 a 06/06/2022",
-        "image":" ",
+        "image":"https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
 
       },
       {
         "title": "Show: 'Hard and Heavy'",
         "description": "Show da banda Hard and Heavy | Dia 08/06/2022",
-        "image":" ",
+        "image":"https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
 
       },
       {
         "title": "Chopp 300ml gratuito",
         "description": "Chopp 300ml gratuito para primeira visita como assinante do Beagapp",
-        "image":" ",
+        "image":"https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
       },
       {
         "title": "Chopp 300ml gratuito",
         "description": "Chopp 300ml gratuito para primeira visita como assinante do Beagapp",
-        "image":" ",
+        "image":"https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
       },
       {
         "title": "Show: 'Hard and Heavy'",
         "description": "Show da banda Hard and Heavy | Dia 08/06/2022",
-        "image":" ",
-
+        "image":"https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
       }
     ]
   }
@@ -37,7 +36,6 @@ localStorage.setItem('db', JSON.stringify(jsonObject));
 
 $(document).ready(function () {
     jsonObject.products.forEach((i) => loadProducts(i));
-
 });
 
 
@@ -46,28 +44,30 @@ function loadProducts(data) {
     var li = document.createElement('li');
 
     li.innerHTML =
-        '<div class="card mt-1">' +
-        '<div class="product-1 align-items-right p-2 text-right"> <img src="' + data.image + '" class="rounded" width="150" height="100">' +
-        '<h6 class="mt-2 font-weight-bold mb-2 info">' + data.title + '</h6>' +
-        '<div class="mt-3 info"> <span class="text1 d-block">' + data.description + ' </span> </div>' +
-        '</div>' +
-        '</div>' +
-        '<div class="button-color p-2 text-right text-black"> <span class="text-uppercase ">Editar</span> </div>' +
-        '<div class="button-color p-2 text-right text-black"> <span class="text-uppercase ">Excluir</span> </div>' +
-        '</div>';
+    '<div class="col-12 col-sm-12 col-md-6 col-xl-3 col-xxl-3">' + 
+      '<div class="card">' +
+          '<img class="card-img-top" src="' + data.image + '" alt="Card image cap">' +
+          '<div class="card-body">' +
+              '<h5 class="card-title">' + data.title +'</h5>' +
+              '<p class="card-text"><b>Validade:</b> 31/12/2022</p>' + 
+              '<p class="card-text">'+ data.description +'</p>' +
+          '</div>' +
+          '<div class="btn-group" role="group">' +
+              '<button class="btn btncolor w-50" type="button" data-bs-toggle="modal" data-bs-target="#exampleModalToggle"><b>Editar</b></button>' +
+              '<button class="btn btn-danger w-50" type="button" onclick="deleteProduct(this)"><b>Excluir</b></button>' +
+          '</div>' +
+      '</div>' +
+    '</div>'
 
     document.getElementById("home_product").appendChild(li);
 
 }
 
 
-li.innerHTML = '<div class="card mt-1">' +
-    '<div class="product-1 align-items-right p-2 text-right"> <img src="' + data.image + '" class="rounded" width="150" height="100">' +
-    '<h6 class="mt-2 font-weight-bold mb-2 info">' + data.title + '</h6>' +
-    '<div class="mt-3 info"> <span class="text1 d-block">' + data.description + ' </span> </div>' +
-    '</div>' +
-    '</div>' +
-    '<div class="button-color p-2 text-right text-black"> <span class="text-uppercase ">Editar</span> </div>' +
-    '<div class="button-color p-2 text-right text-black"> <span class="text-uppercase ">Excluir</span> </div>' +
-    '</div>';
+function deleteProduct(item) {
+  var itemIndex = item.closest('li');
+  itemIndex.remove();
+}
+
+
 
